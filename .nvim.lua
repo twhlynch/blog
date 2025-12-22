eda3e50ea9f691f2ccc7d2ab73e50f7054bd18ca
Tom Lynch <twhlynch@gmail.com>
2025-12-22 14:29:02 +1100

    .nvim.lua file

---
+++ .nvim.lua
@@ -0,0 +1,5 @@
+vim.keymap.set("n", "<leader>B", function()
+	local command = "open http://localhost:4321/ && npm run dev"
+	vim.fn.system("tmux kill-window -t npm 2>/dev/null")
+	vim.fn.system("tmux new-window -d -n npm '" .. command .. "'")
+end, { desc = "Build & Run" })
